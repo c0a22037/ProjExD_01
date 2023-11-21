bacd35317b89630c2e9bb04aebbb26195876b18c
C0A22037 <c0a2203782@edu.teu.ac.jp>
2023-11-21 17:34:40 +0900

    演習課題終了

--- pygame_image.py
+++ pygame_image.py
@@ -9,17 +9,18 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#surfaceの生成
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_img = [kk_img, pg.transform.rotozoom(kk_img,10, 1.0)]
+    kk_img = [kk_img, pg.transform.rotozoom(kk_img,1, 1.0), pg.transform.rotozoom(kk_img,2, 1.0), pg.transform.rotozoom(kk_img,3, 1.0), pg.transform.rotozoom(kk_img,4, 1.0), pg.transform.rotozoom(kk_img,5, 1.0), pg.transform.rotozoom(kk_img,6, 1.0) ,pg.transform.rotozoom(kk_img,7, 1.0), pg.transform.rotozoom(kk_img,8, 1.0), pg.transform.rotozoom(kk_img,9, 1.0), pg.transform.rotozoom(kk_img,10, 1.0)]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x =(tmr%1600)
-        screen.blit(bg_img, [-x, 0])#背景画像の生成
-        screen.blit(bg_img, [1600-x, 0])#練習6動く背景画像のつなぎ目
-        screen.blit(kk_img[tmr%2], [300, 200])
+        x =(tmr%3200)
+        screen.blit(bg_img, [-x,0])#背景画像の生成
+        screen.blit(pg.transform.flip(bg_img, True,False),[1600-x, 0])#練習6動く背景画像のつなぎ目
+        screen.blit(bg_img, [3200-x,0])
+        screen.blit(kk_img[tmr%11], [300, 200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1       
         clock.tick(100)
 
 
